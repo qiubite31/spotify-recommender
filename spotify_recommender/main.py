b95c4f92446455f033de8b462d41e8e6bf89b86c
qiubite31 <qiubite31@gmail.com>
2019-03-17 22:16:52 +0800

    merge get token and client into one function

--- spotify_recommender/main.py
+++ spotify_recommender/main.py
@@ -17,12 +17,17 @@ auth_info = {
 }
 
 
-def get_authorized_token(scope, user_name, client_id, client_secret, redirect_uri):
+def get_authorized_client(scope, user_name, client_id, client_secret, redirect_uri):
     token = util.prompt_for_user_token(user_name, scope,
                                        client_id=client_id,
                                        client_secret=client_secret,
                                        redirect_uri=redirect_uri)
-    return token
+    if token:
+        sp = spotipy.Spotify(auth=token)
+    else:
+        print("Can't get token for", user_name)
+
+    return sp
 
 
 def extract_track_info(items):
@@ -86,73 +91,62 @@ def get_audio_features(sp, trackids):
 
 if __name__ == "__main__":
 
-    token = get_authorized_token('user-top-read', **auth_info)
+    sp = get_authorized_client('user-top-read', **auth_info)
+    # 取得每首歌的歌曲資訊
+    user_top_track_df = get_user_top_tracks(sp, period='all').drop_duplicates()
+    print('You have {} top songs'.format(len(user_top_track_df)))
 
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        # 取得每首歌的歌曲資訊
-        user_top_track_df = get_user_top_tracks(sp, period='all').drop_duplicates()
-        print('You have {} top songs'.format(len(user_top_track_df)))
+    # 使用id取得每首歌的音樂特徵值
+    feature_df = get_audio_features(sp, user_top_track_df['id'].tolist())
+    # 合併歌曲資訊與音樂特徵值
+    user_track_df = pd.merge(user_top_track_df, feature_df, on='id', how='left')
 
-        # 使用id取得每首歌的音樂特徵值
-        feature_df = get_audio_features(sp, user_top_track_df['id'].tolist())
-        # 合併歌曲資訊與音樂特徵值
-        user_track_df = pd.merge(user_top_track_df, feature_df, on='id', how='left')
+    keyword = '台灣流行樂'
+    owner = 'Spotify'
+    playlists = sp.search(q=keyword, type='playlist')['playlists']['items']
+    for playlist in playlists:
+        if playlist['name'] == keyword and \
+            playlist['owner']['display_name'] == owner:
+            playlist_id = playlist['id']
+            owner_id = playlist['owner']['id']
+            break
 
-        keyword = '台灣流行樂'
-        owner = 'Spotify'
-        playlists = sp.search(q=keyword, type='playlist')['playlists']['items']
-        for playlist in playlists:
-            if playlist['name'] == keyword and \
-                playlist['owner']['display_name'] == owner:
-                playlist_id = playlist['id']
-                owner_id = playlist['owner']['id']
-                break
+    # 取得待推薦的歌曲清單
+    track_items = sp.user_playlist(owner_id, playlist_id)['tracks']['items']
+    track_items = [item['track'] for item in track_items]
+    tw_track_df = extract_track_info(track_items)
+    # 取得特徵值並合住
+    tw_track_feature_df = get_audio_features(sp, tw_track_df['id'].tolist())
+    tw_track_df = pd.merge(tw_track_df, tw_track_feature_df, on='id', how='left')
 
-        # 取得待推薦的歌曲清單
-        track_items = sp.user_playlist(owner_id, playlist_id)['tracks']['items']
-        track_items = [item['track'] for item in track_items]
-        tw_track_df = extract_track_info(track_items)
-        # 取得特徵值並合住
-        tw_track_feature_df = get_audio_features(sp, tw_track_df['id'].tolist())
-        tw_track_df = pd.merge(tw_track_df, tw_track_feature_df, on='id', how='left')
+    # 將user向量和item向量合併作rescale並計算相似度
+    user_vec = user_track_df.drop(['album', 'name', 'artist', 'artist_id', 'popularity', 'duration_ms'], axis=1).set_index('id').mean().as_matrix()
 
-        # 將user向量和item向量合併作rescale並計算相似度
-        user_vec = user_track_df.drop(['album', 'name', 'artist', 'artist_id', 'popularity', 'duration_ms'], axis=1).set_index('id').mean().as_matrix()
+    item_vec = tw_track_df.drop(['album', 'name', 'artist', 'artist_id', 'popularity', 'duration_ms'], axis=1).set_index('id').as_matrix()
 
-        item_vec = tw_track_df.drop(['album', 'name', 'artist', 'artist_id', 'popularity', 'duration_ms'], axis=1).set_index('id').as_matrix()
+    scaler = StandardScaler()
+    scaled_matrix = scaler.fit_transform(np.append(user_vec, item_vec).reshape(101, 12))
+    user_vec, item_vec = scaled_matrix[0], scaled_matrix[1:]
+    sim_vec = np.linalg.norm(item_vec-user_vec, ord=2, axis=1)
+    tw_track_df['Score'] = sim_vec
+    tw_track_df = tw_track_df.sort_values('Score', ascending=False)
+    add_tracks = tw_track_df.iloc[:10]['id'].tolist()
 
-        scaler = StandardScaler()
-        scaled_matrix = scaler.fit_transform(np.append(user_vec, item_vec).reshape(101, 12))
-        user_vec, item_vec = scaled_matrix[0], scaled_matrix[1:]
-        sim_vec = np.linalg.norm(item_vec-user_vec, ord=2, axis=1)
-        tw_track_df['Score'] = sim_vec
-        tw_track_df = tw_track_df.sort_values('Score', ascending=False)
-        add_tracks = tw_track_df.iloc[:10]['id'].tolist()
+    splist = get_authorized_client('playlist-modify-public', **auth_info)
+
+    user_id = sp.current_user()['id']
+    # 搜尋是否推薦清單已存在
+    is_list_create = False
+    for list in sp.current_user_playlists(limit=50)['items']:
+        if list['name'] == 'Recommendation':
+            list_id = list['id']
+            is_list_create = True
+            rm_tracks = [track['track']['id'] for track in sp.user_playlist_tracks(user_id, list_id)['items']]
+            break
+
+    if is_list_create:
+        splist.user_playlist_remove_all_occurrences_of_tracks(user_id, list_id, rm_tracks)
     else:
-        print("Can't get token for", username)
+        splist.user_playlist_create(user_id, 'Recommendation', public=True)
 
-    token = get_authorized_token('playlist-modify-public', **auth_info)
-
-    if token:
-        splist = spotipy.Spotify(auth=token)
-
-        user_id = sp.current_user()['id']
-        # 搜尋是否推薦清單已存在
-        is_list_create = False
-        for list in sp.current_user_playlists(limit=50)['items']:
-            if list['name'] == 'Recommendation':
-                list_id = list['id']
-                is_list_create = True
-                rm_tracks = [track['track']['id'] for track in sp.user_playlist_tracks(user_id, list_id)['items']]
-                break
-
-        if is_list_create:
-            splist.user_playlist_remove_all_occurrences_of_tracks(user_id, list_id, rm_tracks)
-        else:
-            splist.user_playlist_create(user_id, 'Recommendation', public=True)
-
-        splist.user_playlist_add_tracks(user_id, list_id, add_tracks, position=None)
-
-    else:
-        print("Can't get token for", username)
+    splist.user_playlist_add_tracks(user_id, list_id, add_tracks, position=None)
