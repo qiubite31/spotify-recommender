dade2e177b1d0fdcecabf13703f78e4e0c40124b
qiubite31 <qiubite31@gmail.com>
2019-03-18 21:33:21 +0800

    Extract recommend by top track / refresh playlist

--- spotify_recommender/main.py
+++ spotify_recommender/main.py
@@ -89,7 +89,7 @@ def get_audio_features(sp, trackids):
     return feature_df
 
 
-def get_recommendate_tracks(user_track_df, tw_track_df):
+def get_recommended_by_user_profile(user_track_df, tw_track_df):
     # 將user向量和item向量合併作rescale並計算相似度
     user_vec = user_track_df.drop(['album', 'name', 'artist', 'artist_id', 'popularity', 'duration_ms'], axis=1).set_index('id').mean().as_matrix()
 
@@ -104,6 +104,46 @@ def get_recommendate_tracks(user_track_df, tw_track_df):
     add_tracks = tw_track_df.iloc[:10]['id'].tolist()
 
     return add_tracks
+
+
+def get_recommended_by_all_top_tracks(user_track_df, tw_track_df):
+    user_vec = user_track_df.drop(['album', 'name', 'artist', 'artist_id', 'popularity', 'duration_ms'], axis=1).set_index('id').as_matrix()
+    item_vec = tw_track_df.drop(['album', 'name', 'artist', 'artist_id', 'popularity', 'duration_ms'], axis=1).set_index('id').as_matrix()
+
+    scaler = StandardScaler()
+    scaled_matrix = scaler.fit_transform(np.append(user_vec, item_vec).reshape(len(user_track_df)+100, 12))
+
+    user_vec, item_vec = scaled_matrix[:len(user_track_df)], scaled_matrix[len(user_track_df):]
+
+    vote_tracks = []
+    for idx in range(len(user_track_df)):
+        sim_vec = np.linalg.norm(item_vec-user_vec[idx], ord=2, axis=1)
+        top10_tracks = pd.Series(sim_vec, index=tw_track_df['id']).sort_values(ascending=False)[:10].index.tolist()
+        vote_tracks += top10_tracks
+
+    from collections import Counter
+    vote_tracks_count = Counter(vote_tracks)
+    add_tracks = [track[0] for track in vote_tracks_count.most_common(10)]
+
+    return add_tracks
+
+
+def refresh_recommended_playlist(sp, splist, user_id, playlist_name, add_tracks):
+    # 搜尋是否推薦清單已存在
+    is_list_create = False
+    for list in sp.current_user_playlists(limit=50)['items']:
+        if list['name'] == playlist_name:
+            list_id = list['id']
+            is_list_create = True
+            rm_tracks = [track['track']['id'] for track in sp.user_playlist_tracks(user_id, list_id)['items']]
+            break
+
+    if is_list_create:
+        splist.user_playlist_remove_all_occurrences_of_tracks(user_id, list_id, rm_tracks)
+    else:
+        splist.user_playlist_create(user_id, playlist_name, public=True)
+
+    splist.user_playlist_add_tracks(user_id, list_id, add_tracks, position=None)
 
 
 if __name__ == "__main__":
@@ -136,38 +176,10 @@ if __name__ == "__main__":
     tw_track_feature_df = get_audio_features(sp, tw_track_df['id'].tolist())
     tw_track_df = pd.merge(tw_track_df, tw_track_feature_df, on='id', how='left')
 
-    user_vec = user_track_df.drop(['album', 'name', 'artist', 'artist_id', 'popularity', 'duration_ms'], axis=1).set_index('id').as_matrix()
-    item_vec = tw_track_df.drop(['album', 'name', 'artist', 'artist_id', 'popularity', 'duration_ms'], axis=1).set_index('id').as_matrix()
-
-    scaler = StandardScaler()
-    scaled_matrix = scaler.fit_transform(np.append(user_vec, item_vec).reshape(len(user_track_df)+100, 12))
-
-    user_vec, item_vec = scaled_matrix[:len(user_track_df)], scaled_matrix[len(user_track_df):]
-
-    vote_tracks = []
-    for idx in range(len(user_track_df)):
-        sim_vec = np.linalg.norm(item_vec-user_vec[idx], ord=2, axis=1)
-        top10_tracks = pd.Series(sim_vec, index=tw_track_df['id']).sort_values(ascending=False)[:10].index.tolist()
-        vote_tracks += top10_tracks
-
-    from collections import Counter
-    vote_tracks_count = Counter(vote_tracks)
-    add_tracks = [track[0] for track in vote_tracks_count.most_common(10)]
+    add_tracks = get_recommended_by_all_top_tracks(user_track_df, tw_track_df)
 
     splist = get_authorized_client('playlist-modify-public', **auth_info)
     user_id = sp.current_user()['id']
-    # 搜尋是否推薦清單已存在
-    is_list_create = False
-    for list in sp.current_user_playlists(limit=50)['items']:
-        if list['name'] == 'Recommendation':
-            list_id = list['id']
-            is_list_create = True
-            rm_tracks = [track['track']['id'] for track in sp.user_playlist_tracks(user_id, list_id)['items']]
-            break
+    playlist_name = 'Recommendation'
 
-    if is_list_create:
-        splist.user_playlist_remove_all_occurrences_of_tracks(user_id, list_id, rm_tracks)
-    else:
-        splist.user_playlist_create(user_id, 'Recommendation', public=True)
-
-    splist.user_playlist_add_tracks(user_id, list_id, add_tracks, position=None)
+    refresh_recommended_playlist(sp, splist, user_id, playlist_name, add_tracks)
