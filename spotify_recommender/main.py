dfd0cb0700a7c4068a6c763e29760fc8242925ca
qiubite31 <qiubite31@gmail.com>
2019-03-21 21:32:45 +0800

    move refresh playlist to utils

--- spotify_recommender/main.py
+++ spotify_recommender/main.py
@@ -7,6 +7,7 @@ import spotipy.util as util
 from sklearn.preprocessing import StandardScaler
 from recommendation import TrackContentBasedFiltering
 from util import SpotifyClientAuthorization
+from util import refresh_recommended_playlist
 
 config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
@@ -19,24 +20,6 @@ auth_info = {
 }
 
 
-def refresh_recommended_playlist(sp, splist, user_id, playlist_name, add_tracks):
-    # 搜尋是否推薦清單已存在
-    is_list_create = False
-    for list in sp.current_user_playlists(limit=50)['items']:
-        if list['name'] == playlist_name:
-            list_id = list['id']
-            is_list_create = True
-            rm_tracks = [track['track']['id'] for track in sp.user_playlist_tracks(user_id, list_id)['items']]
-            break
-
-    if is_list_create:
-        splist.user_playlist_remove_all_occurrences_of_tracks(user_id, list_id, rm_tracks)
-    else:
-        splist.user_playlist_create(user_id, playlist_name, public=True)
-
-    splist.user_playlist_add_tracks(user_id, list_id, add_tracks, position=None)
-
-
 if __name__ == "__main__":
 
     auth = SpotifyClientAuthorization(**auth_info)
@@ -44,6 +27,7 @@ if __name__ == "__main__":
     query_info = {'keyword': '台灣流行樂',
                   'owner': 'Spotify'}
 
+    playlist_name = 'Recommendation'
     track = TrackContentBasedFiltering(auth,
                                        user_track_source='saved_track',
                                        user_content='profile',
@@ -52,9 +36,7 @@ if __name__ == "__main__":
 
     recommended_tracks = track.recommend()
 
-    sp = list(track.spotify_clients.values())[0]
-    splist = auth.get_authorized_client('playlist-modify-public')
+    sp = auth.get_authorized_client('playlist-modify-public')
     user_id = sp.current_user()['id']
-    playlist_name = 'Recommendation'
 
-    refresh_recommended_playlist(sp, splist, user_id, playlist_name, recommended_tracks)
+    refresh_recommended_playlist(sp, user_id, playlist_name, recommended_tracks)
--- spotify_recommender/util.py
+++ spotify_recommender/util.py
@@ -21,3 +21,21 @@ class SpotifyClientAuthorization:
             print("Can't get token for", self.user_name)
 
         return sp
+
+
+def refresh_recommended_playlist(sp, user_id, playlist_name, add_tracks):
+    # 搜尋是否推薦清單已存在
+    is_list_create = False
+    for list in sp.current_user_playlists(limit=50)['items']:
+        if list['name'] == playlist_name:
+            list_id = list['id']
+            is_list_create = True
+            rm_tracks = [track['track']['id'] for track in sp.user_playlist_tracks(user_id, list_id)['items']]
+            break
+
+    if is_list_create:
+        sp.user_playlist_remove_all_occurrences_of_tracks(user_id, list_id, rm_tracks)
+    else:
+        sp.user_playlist_create(user_id, playlist_name, public=True)
+
+    sp.user_playlist_add_tracks(user_id, list_id, add_tracks, position=None)
